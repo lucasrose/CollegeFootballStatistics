058c8d65df1e44004e32c1d40cdfbec4438d42bc
Lucas Rose <lucasr95@vt.edu>
2017-04-07 15:16:55 -0400

    Setup Completed

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express')
+  , logger = require('morgan')
+  , app = express()
+  , path = __dirname + "/static/source/templates/"
+
+var router = express.Router()
+
+app.use(logger('dev'))
+app.use(express.static(__dirname + '/static'))
+
+router.use(function (req, res, next) {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get('/', function (req, res, next) {
+  res.sendFile(path + "index.html")
+})
+
+app.use("/", router);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
+})
