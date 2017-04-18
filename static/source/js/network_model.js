a7ac4dacb4ae283d81c16980be387799f7ad3e12
Lucas Rose <lucasr95@vt.edu>
2017-04-18 13:18:50 -0400

    Working on Formatting proper data

--- static/source/js/network_model.js
+++ static/source/js/network_model.js
@@ -1,62 +1,26 @@
 //append table information
 $(document).ready(function(){
-  for(var i = 0; i < 25; i++){
-      insertTeam()
-  }
+  getRankings2015()
+  
 })
 
-////pass sorted array of leagues
-//function createTable(data){
-//  var tableHTML = '<div id="league-table">'
-//  var leagues = ["ACC", "SEC", "Big-10", "Big-12", "Pac-12"]
-//  
-//  for (var i = 0; i < leagues.length; i++){
-//    tableHTML += createLeague(data, leagues[i])
-//  }
-//  
-//  tableHTML += '</div>'
-//  return tableHTML
-//}
-//
-////pass json of league, create league from teams
-//function createLeague(data, league){
-//  var leagueHTML = '<div id="' + league +'">'
-//  
-//  for (var i = 0; i < 25, i++){
-//    var team = data[i]
-//    if (team.league === league)
-//      leagueHTML += createTeam(team)
-//  }
-//  
-//  leagueHTML += '</div>'
-//  return leagueHTML
-//}
-//
-////pass single team data, build html from json
-//function createTeam(data){
-//  var team = data.name  //arbitrary data.name
-//  var logo = data.link  //arbitrary data.link
-//  var html = '<a href="' + team '">' +
-//             '<img src="'+ logo '"></a>'
-//             
-//  return html
-//}
-
-function insertTeam(){
+function insertTeam(team){
   var html = "<li class='team'>" +
-             "<div><a href='team_1' ><img " +            "src='http://maxpreps.cbsistatic.com/includes/images/signing_day/logos/site/virginia_tech.png'></a>" +
+             "<div><a href='" + team + "' ><img " +            "src='http://maxpreps.cbsistatic.com/includes/images/signing_day/logos/site/virginia_tech.png'></a>" +
              "</div></li>"
   $("#inner").append(html)
 }
 
-
-function getRankings2012(){
-  loadFile("http://34.208.59.48:8000/api/rankings/2012", getResults, "Rankings_2012\n")
+function getRankings2015(){
+  loadFile("http://34.208.59.48:8000/api/our-rankings/2015/", getResults)
 }
 
-function getResults(json){
-  //var parsedJSON = JSON.parse(json)
-  return (json + this.responseText)
+function getResults(){
+  var json = this.response
+  var CFB25 = json["0"]
+  var AP25 = json["1"]
+  
+  alert(CFB25.type)
 }
 
 function xhrSuccess () { this.callback.apply(this, this.arguments) }
@@ -64,12 +28,13 @@ function xhrSuccess () { this.callback.apply(this, this.arguments) }
 function xhrError () { console.error(this.statusText) }
 
 function loadFile (sURL, fCallback /*, argumentToPass1, argumentToPass2, etc. */) {
-  var oReq = new XMLHttpRequest()
-  oReq.callback = fCallback
-  oReq.arguments = Array.prototype.slice.call(arguments, 2)
-  oReq.onload = xhrSuccess
-  oReq.onerror = xhrError
-  oReq.open("get", sURL, true)
-  oReq.send(null)
+  var xhr = new XMLHttpRequest()
+  xhr.callback = fCallback
+  xhr.arguments = Array.prototype.slice.call(arguments, 2)
+  xhr.responseType = 'json'
+  xhr.onload = xhrSuccess
+  xhr.onerror = xhrError
+  xhr.open("get", sURL, true)
+  xhr.send(null)
 }
 
