d18fda10ad85e55ec43fd3532bace1f72229a6f3
Lucas Rose <lucasr95@vt.edu>
2017-04-20 21:04:10 -0400

    Styled Bios and Added JS

--- static/source/js/about_us.js
+++ static/source/js/about_us.js
@@ -1,11 +1,51 @@
-function createPerson(name, major, bio){
+$(document).ready(function(){
+  addGrant()
+  addKyle()
+  addLucas()
+})
+
+function createPerson(name, major, bio, linkedInUrl, gitHubUrl){
   var html = "<div class='person'>" +
     "<div>" +
       "<h2 class='page-sub-header'>" + name + 
         "<div class='major'>" + major + " - VIRGINIA TECH</div>" +
-        "</h2>" +
+        "<a class='linkedin-link' href='" + linkedInUrl + "'>LinkedIn</a>" +
+        "<a class='github-link' href='" + gitHubUrl + "'>GitHub</a>" + 
+      "</h2>" +
     "</div>" +
 "<div class='bio'>" + bio + "</div></div>"
   
   $('#inner').append(html)
+}
+
+
+
+function addLucas(){
+  var name = "Lucas Rose"
+  var major = "COMPUTER SCIENCE"
+  var linkedInUrl = ""
+  var gitHubUrl = ""
+  var bio = "Insert Bio Here"
+  
+  createPerson(name, major, bio, linkedInUrl, gitHubUrl)
+}
+
+function addKyle(){
+  var name = "Kyle Johnson"
+  var major = "COMPUTER SCIENCE AND MATHEMATICS"
+  var linkedInUrl = ""
+  var gitHubUrl = ""
+  var bio = "Insert Bio Here"
+  
+  createPerson(name, major, bio, linkedInUrl, gitHubUrl)
+}
+
+function addGrant(){
+  var name = "Grant Hughes"
+  var major = "COMPUTER SCIENCE"
+  var linkedInUrl = ""
+  var gitHubUrl = ""
+  var bio = "Insert Bio Here"
+  
+  createPerson(name, major, bio, linkedInUrl, gitHubUrl)
 }
